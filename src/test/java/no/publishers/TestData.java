254bce413d291695e7d11edb046715d05cf43ff0
NilsOveTen <nils.ove.tendenes@acando.no>
2019-06-17 13:13:25 +0200

    Unit test PublisherService

--- src/test/java/no/publishers/TestData.java
+++ src/test/java/no/publishers/TestData.java
@@ -1,6 +1,9 @@
 package no.publishers;
 
 import no.publishers.generated.model.Publisher;
+import no.publishers.graphql.CreatePublisher;
+import no.publishers.model.PublisherDB;
+import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,4 +16,23 @@ public class TestData {
     public static List<Publisher> emptyPublisherList = new ArrayList<>();
 
     public static List<Publisher> publishers = Collections.singletonList(publisher);
+
+    public static PublisherDB persistedPublisher = createPublisherDB();
+
+    private static PublisherDB createPublisherDB() {
+        PublisherDB newPublisherDB = new PublisherDB();
+
+        newPublisherDB.setId(new ObjectId());
+        newPublisherDB.setName("name");
+        newPublisherDB.setUri("uri");
+        newPublisherDB.setOrganizationId("orgId");
+        newPublisherDB.setOrgPath("orgPath");
+        newPublisherDB.setPrefLabel("prefLabel");
+
+        return newPublisherDB;
+    }
+
+    public static CreatePublisher publisherInput = new CreatePublisher(
+        "name", "uri", "orgId", "orgPath", "prefLabel"
+    );
 }
