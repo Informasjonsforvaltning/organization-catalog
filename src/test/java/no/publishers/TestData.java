8e8ca080001a5b9a9a613d80e2646f89eb57e7e6
NilsOveTen <nils.ove.tendenes@acando.no>
2019-06-17 12:30:04 +0200

    Unit test PublishersApi

--- src/test/java/no/publishers/TestData.java
+++ src/test/java/no/publishers/TestData.java
@@ -1,4 +1,16 @@
 package no.publishers;
 
+import no.publishers.generated.model.Publisher;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TestData {
+
+    public static Publisher publisher = new Publisher();
+
+    public static List<Publisher> emptyPublisherList = new ArrayList<>();
+
+    public static List<Publisher> publishers = Collections.singletonList(publisher);
 }
