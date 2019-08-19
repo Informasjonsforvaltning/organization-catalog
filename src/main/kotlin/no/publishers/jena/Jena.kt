579fda24368e8791423216a0cd61386adf06ccb3
NilsOveTen <nils.ove.tendenes@acando.no>
2019-08-19 15:01:33 +0200

    BR namespace

--- src/main/kotlin/no/publishers/jena/Jena.kt
+++ src/main/kotlin/no/publishers/jena/Jena.kt
@@ -2,18 +2,15 @@ package no.publishers.jena
 
 import no.publishers.generated.model.PrefLabel
 import no.publishers.generated.model.Publisher
-import no.publishers.jena.adms.ADMS
 import org.apache.jena.rdf.model.Model
 import org.apache.jena.rdf.model.ModelFactory
 import org.apache.jena.rdf.model.Property
 import org.apache.jena.rdf.model.Resource
 import org.apache.jena.sparql.vocabulary.FOAF
-import org.apache.jena.vocabulary.DCAT
 import org.apache.jena.vocabulary.DCTerms
 import org.apache.jena.vocabulary.RDF
 import org.apache.jena.vocabulary.ROV
 import org.apache.jena.vocabulary.SKOS
-import org.apache.jena.vocabulary.SKOSXL
 import java.io.ByteArrayOutputStream
 
 fun Publisher.jenaResponse(acceptHeader: String?): String =
@@ -28,60 +25,50 @@ fun List<Publisher>.jenaResponse(acceptHeader: String?): String =
 private fun List<Publisher>.createModel(): Model {
     val model = ModelFactory.createDefaultModel()
     model.setNsPrefix("dct", DCTerms.getURI())
-    model.setNsPrefix("dcat", DCAT.getURI())
-    model.setNsPrefix("skosxl", SKOSXL.uri)
     model.setNsPrefix("skos", SKOS.uri)
     model.setNsPrefix("foaf", FOAF.getURI())
     model.setNsPrefix("rov", ROV.getURI())
     model.setNsPrefix("adms", ADMS.uri)
+    model.setNsPrefix("br", BR.uri)
 
     forEach {
         model.createResource(it.uri)
-            .addProperty(RDF.type, model.createResource(FOAF.Organization))
+            .addProperty(RDF.type, FOAF.Organization)
             .addProperty(DCTerms.identifier, it.id)
-            .safeAddProperty(FOAF.name, it.name)
-            .addProperty(
-                ROV.registration,
-                model.createResource(ADMS.Identifier)
-                    .safeAddProperty(DCTerms.issued, it.issued?.toString())
-                    .addProperty(SKOS.notation, it.organizationId)
-                    .addProperty(ADMS.schemaAgency, "Brønnøysundregistrene"))
-            .safeAddProperty(DCTerms.format, it.orgForm)
-            .safeAddProperty(SKOS.mappingRelation, it.orgPath)
-            .safeAddProperty(DCTerms.isPartOf, it.orgParent)
-            .safeAddProperty(SKOS.historyNote, it.municipalityNumber)
-            .addProperty(
-                FOAF.interest,
-                model.createResource(SKOSXL.Label)
-                    .safeAddProperty(DCAT.accessURL, it.industryCode?.uri)
-                    .safeAddProperty(FOAF.nick, it.industryCode?.code)
-                    .addProperty(
-                        SKOSXL.prefLabel,
-                        model.createResource(SKOSXL.Label).addPrefLabels(it.industryCode?.prefLabel ?: PrefLabel())))
-            .addProperty(
-                FOAF.schoolHomepage,
-                model.createResource(SKOSXL.Label)
-                    .safeAddProperty(DCAT.accessURL, it.sectorCode?.uri)
-                    .safeAddProperty(FOAF.nick, it.sectorCode?.code)
-                    .addProperty(
-                        SKOSXL.prefLabel,
-                        model.createResource(SKOSXL.Label).addPrefLabels(it.sectorCode?.prefLabel ?: PrefLabel())))
-            .addProperty(
-                SKOSXL.prefLabel,
-                model.createResource(SKOSXL.Label).addPrefLabels(it.prefLabel))
+            .safeAddProperty(ROV.legalName, it.name)
+            .addRegistration(it)
+            .safeAddProperty(BR.organisasjonsform, it.orgForm)
+            .safeAddProperty(BR.orgPath, it.orgPath)
+            .safeAddProperty(BR.kommunenummer, it.municipalityNumber)
+            .safeAddLinkedProperty(BR.overordnetEnhet, it.orgParent)
+            .safeAddLinkedProperty(BR.naeringskode, it.industryCode.uri)
+            .safeAddLinkedProperty(BR.sektorkode, it.sectorCode.uri)
+            .addPreferredNames(it.prefLabel)
     }
 
     return model
 }
 
+private fun Resource.addRegistration(publisher: Publisher): Resource =
+    addProperty(
+        ROV.registration,
+        model.createResource(ADMS.Identifier)
+            .safeAddProperty(DCTerms.issued, publisher.issued?.toString())
+            .addProperty(SKOS.notation, publisher.organizationId)
+            .addProperty(ADMS.schemaAgency, "Brønnøysundregistrene"))
+
 private fun Resource.safeAddProperty(property: Property, value: String?): Resource =
     if(value == null) this
     else addProperty(property, value)
 
-private fun Resource.addPrefLabels(prefLabel: PrefLabel):Resource {
-    if (prefLabel.nb != null) addProperty(SKOSXL.literalForm, prefLabel.nb, "nb")
-    if (prefLabel.nn != null) addProperty(SKOSXL.literalForm, prefLabel.nn, "nn")
-    if (prefLabel.en != null) addProperty(SKOSXL.literalForm, prefLabel.en, "en")
+private fun Resource.safeAddLinkedProperty(property: Property, value: String?): Resource =
+    if(value == null) this
+    else addProperty(property, model.createResource(value))
+
+private fun Resource.addPreferredNames(preferredNames: PrefLabel): Resource {
+    if (preferredNames.nb != null) addProperty(FOAF.name, preferredNames.nb, "nb")
+    if (preferredNames.nn != null) addProperty(FOAF.name, preferredNames.nn, "nn")
+    if (preferredNames.en != null) addProperty(FOAF.name, preferredNames.en, "en")
     return this
 }
 private fun Model.createResponseString(responseType: String):String =
