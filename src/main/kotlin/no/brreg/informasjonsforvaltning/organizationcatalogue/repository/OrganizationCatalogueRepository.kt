48a73d137882273c0d8c359da7f44a08be463fbe
NilsOveTen <nils.ove.tendenes@acando.no>
2019-10-11 09:17:15 +0200

    Edit groupid and split docker-compose file

---
+++ src/main/kotlin/no/brreg/informasjonsforvaltning/organizationcatalogue/repository/OrganizationCatalogueRepository.kt
@@ -0,0 +1,11 @@
+package no.brreg.informasjonsforvaltning.organizationcatalogue.repository
+
+import no.brreg.informasjonsforvaltning.organizationcatalogue.model.OrganizationDB
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface OrganizationCatalogueRepository : MongoRepository<OrganizationDB, String> {
+    fun findByNameLike(name: String): List<OrganizationDB>
+    fun findByOrganizationIdLike(organizationId: String): List<OrganizationDB>
+}
