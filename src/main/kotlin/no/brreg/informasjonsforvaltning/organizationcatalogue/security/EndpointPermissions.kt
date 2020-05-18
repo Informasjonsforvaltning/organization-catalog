a300a515ab302a520158fe0db3bd0f25e64d25ae
NilsOveTen <nils.ove.tendenes@acando.no>
2020-05-18 11:31:33 +0200

    chore: update spring boot

--- src/main/kotlin/no/brreg/informasjonsforvaltning/organizationcatalogue/security/EndpointPermissions.kt
+++ src/main/kotlin/no/brreg/informasjonsforvaltning/organizationcatalogue/security/EndpointPermissions.kt
@@ -1,19 +1,17 @@
 package no.brreg.informasjonsforvaltning.organizationcatalogue.security;
 
-import org.springframework.security.core.Authentication
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.stereotype.Component
 
 @Component
 class EndpointPermissions{
-    private fun authentication(): Authentication {
-        return SecurityContextHolder.getContext().authentication
+
+    fun hasAdminPermission(jwt: Jwt?): Boolean {
+        if (jwt == null) return false
+
+        val authorities: String? = jwt.claims["authorities"] as? String
+
+        return authorities?.contains("system:root:admin") ?: false
     }
 
-    fun hasAdminPermission(): Boolean {
-        val requiredAuthority = SimpleGrantedAuthority("system:root:admin")
-
-        return authentication().authorities.contains(requiredAuthority)
-    }
 }
