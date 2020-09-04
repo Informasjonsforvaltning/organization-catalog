b5d2016cc8c09d6d470dbfaaf9489b7e22299163
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-09-04 10:07:21 +0200

    feat: #474 add support for subordinate organizations

--- src/main/kotlin/no/brreg/informasjonsforvaltning/organizationcatalogue/adapter/EnhetsregisteretAdapter.kt
+++ src/main/kotlin/no/brreg/informasjonsforvaltning/organizationcatalogue/adapter/EnhetsregisteretAdapter.kt
@@ -2,12 +2,13 @@ package no.brreg.informasjonsforvaltning.organizationcatalogue.adapter
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import no.brreg.informasjonsforvaltning.organizationcatalogue.configuration.AppProperties
 import no.brreg.informasjonsforvaltning.organizationcatalogue.mapping.createOrgPath
 import no.brreg.informasjonsforvaltning.organizationcatalogue.mapping.cutOrgPathForParents
 import no.brreg.informasjonsforvaltning.organizationcatalogue.model.EnhetsregisteretOrganization
 import org.slf4j.LoggerFactory
-import org.springframework.data.mongodb.core.aggregation.VariableOperators
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import java.io.BufferedReader
@@ -26,25 +27,11 @@ class EnhetsregisteretAdapter(private val appProperties: AppProperties) {
             ?: emptyList()
     }
 
-    private fun downloadAndParseOrganization(organizationId: String): EnhetsregisteretOrganization? {
-        val connection = URL(appProperties.enhetsregisteretProxyUrl + organizationId).openConnection() as HttpURLConnection
+    private fun downloadAndParseOrganization(organizationId: String): EnhetsregisteretOrganization? = runBlocking {
+        val organizationPromise = async { getOrganizationFromEnhetsregisteret(organizationId) }
+        val subordinateOrganizationPromise = async { getOrganizationFromEnhetsregisteret(organizationId, true) }
 
-        if (connection.responseCode != HttpStatus.OK.value()) {
-            LOGGER.error("Organization with id '$organizationId' not found in Enhetsregisteret")
-            return null
-        } else {
-            val jsonBody = connection
-                .inputStream
-                .bufferedReader()
-                .use(BufferedReader::readText)
-
-            return try {
-                jacksonObjectMapper().readValue(jsonBody)
-            } catch (t: Throwable) {
-                LOGGER.error("Unable to parse response from Enhetsregisteret for '$organizationId'")
-                null
-            }
-        }
+        organizationPromise.await() ?: subordinateOrganizationPromise.await()
     }
 
     private fun EnhetsregisteretOrganization.downloadParentOrgsAndCreateOrgPath(): List<EnhetsregisteretOrganization> {
@@ -57,7 +44,7 @@ class EnhetsregisteretAdapter(private val appProperties: AppProperties) {
 
         while (parentOrganizationId != null) {
             idSet.add(parentOrganizationId)
-            val parent = downloadAndParseOrganization(parentOrganizationId)
+            val parent = getOrganizationFromEnhetsregisteret(parentOrganizationId)
             if (parent != null) {
                 topParentOrgForm = parent.organisasjonsform?.kode
                 parentOrganizationId = parent.overordnetEnhet
@@ -71,6 +58,27 @@ class EnhetsregisteretAdapter(private val appProperties: AppProperties) {
             it.copy(orgPath = cutOrgPathForParents(completeOrgPath, it.organisasjonsnummer))
         }
     }
+
+    private fun getOrganizationFromEnhetsregisteret(organizationId: String, isSubordinate: Boolean = false): EnhetsregisteretOrganization? =
+        URL("${appProperties.enhetsregisteretProxyUrl}/${if (isSubordinate) "underenheter" else "enheter"}/$organizationId")
+            .openConnection()
+            .run {
+                this as HttpURLConnection
+
+                if (responseCode != HttpStatus.OK.value()) {
+                    LOGGER.error("Organization (${if (isSubordinate) "underenhet" else "enhet"}) with id '$organizationId' not found in Enhetsregisteret")
+                    return null
+                }
+
+                val jsonBody = inputStream.bufferedReader().use(BufferedReader::readText)
+
+                return try {
+                    jacksonObjectMapper().readValue(jsonBody)
+                } catch (t: Throwable) {
+                    LOGGER.error("Unable to parse response from Enhetsregisteret for '$organizationId'")
+                    null
+                }
+            }
 
     private fun isTestEnvironment(): Boolean =
         setOf("localhost", "staging", "demo")
