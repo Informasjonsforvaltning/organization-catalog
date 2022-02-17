6ca6eb818d4ffbc07d4070903744b93516db18c7
Nils Ove Tendenes <50194012+NilsOveTen@users.noreply.github.com>
2022-02-17 10:23:00 +0100

    feat: brreg forwarding filter (#114)

--- src/main/kotlin/no/brreg/informasjonsforvaltning/organizationcatalogue/configuration/AppProperties.kt
+++ src/main/kotlin/no/brreg/informasjonsforvaltning/organizationcatalogue/configuration/AppProperties.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.ConstructorBinding
 @ConfigurationProperties("application")
 data class AppProperties(
     val enhetsregisteretUrl: String,
+    val enhetsregisteretHtmlUrl: String,
     val enhetsregisteretProxyUrl: String,
     val organizationCatalogueHost: String,
     val municipalityUrl: String,
---
+++ src/main/kotlin/no/brreg/informasjonsforvaltning/organizationcatalogue/configuration/BrregForwardingFilter.kt
@@ -0,0 +1,42 @@
+package no.brreg.informasjonsforvaltning.organizationcatalogue.configuration
+
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import javax.servlet.Filter
+import javax.servlet.FilterChain
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+private val LOGGER = LoggerFactory.getLogger(BrregForwardingFilter::class.java)
+
+@Component
+class BrregForwardingFilter(private val appProperties: AppProperties) : Filter {
+
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
+        if (request != null && response != null && chain != null) {
+            try {
+                val httpRequest = request as HttpServletRequest
+                val httpResponse = response as HttpServletResponse
+
+                val acceptContainsHtml: Boolean = httpRequest.getHeader("Accept")
+                    ?.contains("text/html")
+                    ?: false
+
+                if (acceptContainsHtml && httpRequest.servletPath.contains("organizations")) {
+                    val orgId = httpRequest.servletPath.substringAfter("organizations/", "")
+                    httpResponse.setHeader("Location", "${appProperties.enhetsregisteretHtmlUrl}$orgId")
+                    httpResponse.status = HttpStatus.SEE_OTHER.value()
+                    return
+                }
+            } catch (ex: Exception) {
+                LOGGER.error("Forwarding filter failed", ex)
+            }
+        }
+
+        chain?.doFilter(request, response)
+    }
+
+}
